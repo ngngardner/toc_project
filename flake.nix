c31d81d5f8af1d70b8c42e3ac533d45b983e2f1b
Noah Gardner <51331526+ngngardner@users.noreply.github.com>
2021-10-03 16:29:09 -0400

    Dev (#3)
    * flake nix
    
    * poetry and notebook
    
    * autopep8 dev
    
    * andy poetry test
    
    Co-authored-by: vespenegas <91216338+vespenegas@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Theory of Computation Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    utils = { url = "github:numtide/flake-utils"; };
+    compat = { url = "github:edolstra/flake-compat"; flake = false; };
+  };
+
+  outputs = { self, nixpkgs, utils, compat }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        python = pkgs.python39;
+        projectDir = ./.;
+        overrides = pkgs.poetry2nix.overrides.withDefaults (final: prev: {
+          # Python dependency overrides go here
+        });
+
+        packageName = "traffic_sim";
+      in
+      {
+        packages.${packageName} = pkgs.poetry2nix.mkPoetryApplication {
+          inherit python projectDir overrides;
+          propogatedBuildInputs = [
+            # Non-Python runtime dependencies go here
+          ];
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            # python
+            (pkgs.poetry2nix.mkPoetryEnv {
+              inherit python projectDir overrides;
+            })
+            pkgs.python39Packages.poetry
+
+            # tex
+            (pkgs.texlive.combine { inherit (pkgs.texlive) 
+              scheme-small 
+              xstring 
+              iftex
+              totpages
+              environ
+              hyperxmp
+            ; })
+          ];
+        };
+      });
+}
--- shell.nix
+++ shell.nix
@@ -1,16 +1,14 @@
-{ pkgs ? import ./nix { } }:
-pkgs.mkShell {
-  packages = [
-    (pkgs.texlive.combine { inherit (pkgs.texlive) 
-      scheme-full 
-      xstring 
-      iftex
-      totpages
-      environ
-      hyperxmp
-    ; })
-
-    # nix dev
-    pkgs.nixpkgs-fmt
-  ];
-}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  {
+    src = ./.;
+  }).shellNix
+  
