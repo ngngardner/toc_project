41947d00c2d7c9d396a774092f797dde03b3e941
Noah Gardner <ngngardner@gmail.com>
2021-10-12 17:07:32 -0400

    fix merge

--- shell.nix
+++ shell.nix
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-{ pkgs }:
-pkgs.mkShell {
-  packages = [
-    (pkgs.texlive.combine { inherit (pkgs.texlive) 
-      scheme-small 
-      xstring 
-      iftex
-      totpages
-      environ
-      hyperxmp
-    ; })
-
-    # nix dev
-    pkgs.nixpkgs-fmt
-  ];
-}
-=======
 (import
   (
     let
@@ -29,5 +11,3 @@ pkgs.mkShell {
   {
     src = ./.;
   }).shellNix
-  
->>>>>>> 837e032a82422226af5209cc4e15e7fb8dc0ab89
